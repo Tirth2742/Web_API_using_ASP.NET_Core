623df962313e3c39e8ea672667952872986a4147
Tirth2742 <tirthofficial2742.com>
2025-02-27 13:18:08 +0530

    login controller

--- webapi/Controllers/AdminController.cs
+++ webapi/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using webapi.Data;
+using webapi.Entities;
 
 namespace webapi.Controllers
 {
@@ -21,5 +22,25 @@ namespace webapi.Controllers
             var admin = await loginData.admins.ToListAsync();
             return Ok(admin);
         }
+        [HttpGet("{userId}/{pwd}")]
+        public async Task<IActionResult> verif(string userId , string pwd)
+        {
+            
+            var admin = await loginData.admins.FirstOrDefaultAsync(a => a.userID == userId);
+            if(admin == null)
+            {
+                return NotFound("user not found");
+            }
+            if (admin.Password == pwd)
+            {
+                return Ok(true);
+            }
+            else
+            {
+                return Unauthorized("Invalid Password");
+            }
+
+        }
+
     }
 }
